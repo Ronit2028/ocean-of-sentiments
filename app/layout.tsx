cce2a12c4d5be938581112c2f19963a4e1d6efb3
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2024-04-19 22:55:18 +0530

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 import { NavbarDemo } from "@/components/navbar";
 import Footer from '@/components/Footer'
 
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
