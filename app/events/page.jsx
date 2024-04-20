efb327b0c7622c28a75b9d8361ab633d860c036c
Your Name <you@example.com>
2024-04-20 10:19:59 +0530

    updates

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,5 +1,6 @@
-import { ParallaxScrollDemo } from '@/components/EventGallery'
-import {TourHeader} from '@/components/TourHeader'
+// import { ParallaxScrollDemo } from '@/components/EventGallery'
+import { ParallaxScrollDemo } from '@/components/ParallaxScroll'
+import { TourHeader } from '@/components/TourHeader'
 import { TracingBeamDemo } from '@/components/TracingBeamDemo'
 
 import React from 'react'
@@ -7,9 +8,10 @@ import React from 'react'
 const page = () => {
   return (
     <main>
-      <TourHeader/>
-      <TracingBeamDemo/>
-      <ParallaxScrollDemo/>
+      <TourHeader />
+      <TracingBeamDemo />
+      <ParallaxScrollDemo />
+      {/* <ParallaxScroll /> */}
     </main>
   )
 }
