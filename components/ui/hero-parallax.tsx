a0f78fde7e32843c709eb61206aa7844e01564ed
Ronit Jai Prakash <128621788+RonitJaiPrakash@users.noreply.github.com>
2025-01-18 04:57:26 +0530

    image source change

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -105,10 +105,10 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Weaving Together Harmony: <br /> The Music of Twin Strings
+        Ocean of <br /> Sentiments
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-      Bringing a fresh perspective to popular music. Creating music that resonates with hearts and gets you moving.
+      Your one stop for video game reviews
       </p>
     </div>
   );
@@ -140,7 +140,7 @@ export const ProductCard = ({
         href={product.link}
         className="block group-hover/product:shadow-2xl "
       >
-        <Image
+        <img
           src={product.thumbnail}
           height="600"
           width="600"
